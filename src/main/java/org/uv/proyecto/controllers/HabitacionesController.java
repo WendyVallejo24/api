00eab9fdfcc8d6ebbee9b5e9923985c0794e52fa
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-06-06 01:37:12 -0600

    permitir conexion

--- src/main/java/org/uv/proyecto/controllers/HabitacionesController.java
+++ src/main/java/org/uv/proyecto/controllers/HabitacionesController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ import org.uv.proyecto.repository.HabitacionesRepository;
  *
  * @author wbpat
  */
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/habitacion")
 public class HabitacionesController {
