afed1cf8a3af153283c264fc2365ea26f7661028
Wendy Vallejo <wbvalpat24@gmail.com>
2023-06-13 16:01:44 -0500

    modificacion de controller de habitaciones

--- src/main/java/org/uv/proyecto/controllers/HabitacionesController.java
+++ src/main/java/org/uv/proyecto/controllers/HabitacionesController.java
@@ -3,14 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.uv.proyecto.controllers;
+
 import java.net.URI;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.uv.proyecto.models.Dispositivos;
 import org.uv.proyecto.models.Habitaciones;
 import org.uv.proyecto.repository.HabitacionesRepository;
 
@@ -28,7 +30,6 @@ import org.uv.proyecto.repository.HabitacionesRepository;
  *
  * @author wbpat
  */
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/habitacion")
 @CrossOrigin(origins = "http://localhost:4200")
@@ -36,9 +37,9 @@ public class HabitacionesController {
 
     @Autowired
     private HabitacionesRepository habitacionRepository;
-    
+
     @GetMapping
-    public ResponseEntity<Page <Habitaciones> > listarHabitaciones(Pageable pageable){
+    public ResponseEntity<Page<Habitaciones>> listarHabitaciones(Pageable pageable) {
         return ResponseEntity.ok(habitacionRepository.findAll(pageable));
     }
 
@@ -59,35 +60,47 @@ public class HabitacionesController {
                 .buildAndExpand(createdHabitacion.getNumero()).toUri();
         return ResponseEntity.created(ubicacion).body(createdHabitacion);
     }
-    
+
     @PutMapping("/{numero}")
-    public ResponseEntity<Habitaciones> actualizarAbitación(@PathVariable Integer numero, @RequestBody Habitaciones habitacion){
+    public ResponseEntity<Habitaciones> actualizarAbitación(@PathVariable Integer numero, @RequestBody Habitaciones habitacion) {
         Optional<Habitaciones> habitacionOptional = habitacionRepository.findById(numero);
-        if(!habitacionOptional.isPresent()){
+        if (!habitacionOptional.isPresent()) {
             return ResponseEntity.unprocessableEntity().build();
         }
         habitacion.setNumero(habitacionOptional.get().getNumero());
         habitacionRepository.save(habitacion);
         return ResponseEntity.noContent().build();
     }
-    
+
     @DeleteMapping("/{numero}")
-    public ResponseEntity<Habitaciones> eliminarHabitacion (@PathVariable Integer numero){
+    public ResponseEntity<Habitaciones> eliminarHabitacion(@PathVariable Integer numero) {
         Optional<Habitaciones> habitacionOptional = habitacionRepository.findById(numero);
-        if(!habitacionOptional.isPresent()){
+        if (!habitacionOptional.isPresent()) {
             return ResponseEntity.unprocessableEntity().build();
         }
-        
+
         habitacionRepository.delete(habitacionOptional.get());
         return ResponseEntity.noContent().build();
     }
-    
+
     @GetMapping("numero")
-    public ResponseEntity<Habitaciones> obtenerHabitacionPorId(@PathVariable Integer numero){
+    public ResponseEntity<Habitaciones> obtenerHabitacionPorId(@PathVariable Integer numero) {
         Optional<Habitaciones> habitacionOptional = habitacionRepository.findById(numero);
-        if(!habitacionOptional.isPresent()){
+        if (!habitacionOptional.isPresent()) {
             return ResponseEntity.unprocessableEntity().build();
         }
         return ResponseEntity.ok(habitacionOptional.get());
+    }
+
+    @GetMapping("/{numero}/dispositivo")
+    public ResponseEntity<Set<Dispositivos>> obtenerDispositivosDeHabitacion(@PathVariable Integer numero) {
+        Optional<Habitaciones> habitacionOptional = habitacionRepository.findById(numero);
+        if (habitacionOptional.isPresent()) {
+            Habitaciones habitacion = habitacionOptional.get();
+            Set<Dispositivos> dispositivos = habitacion.getDispositivos();
+            return ResponseEntity.ok(dispositivos);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
